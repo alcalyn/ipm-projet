5374d83cf2972710c964446d606f187249449313
julien.maulny@gmail.com <julien.maulny@gmail.com@9aab6d8c-2e8b-8920-8967-d62040d22f21>
2013-04-07 16:10:42 +0000

    init JMenu

--- src/views/Menu.java
+++ src/views/Menu.java
@@ -1,10 +1,58 @@
 package views;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Menu extends JMenuBar {
 	
 	private static final long serialVersionUID = -8890344412702786756L;
+	
+	
+	
+	private JMenu		fichier;
+	private JMenuItem		nouveau;
+	
+	
+	private JMenu		affichage;
+	
+	
+	private JMenu		about;
+	
+	
+	public Menu() {
+		super();
+		
+		initFichier();
+		initAffichage();
+		initAbout();
+	}
+	
+	
+	
+	private void initFichier() {
+		fichier = new JMenu("Fichier");
+		nouveau = new JMenuItem("Nouveau");
+		
+		fichier.add(nouveau);
+		
+		fichier.addSeparator();
+		
+		add(fichier);
+	}
+	
+	private void initAffichage() {
+		affichage = new JMenu("Affichage");
+		
+		add(affichage);
+	}
+	
+	private void initAbout() {
+		about = new JMenu("?");
+		
+		add(about);
+	}
+	
 	
 	
 	
