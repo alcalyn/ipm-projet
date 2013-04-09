f13150e7725a270fe14395094c02b9f58e1353be
julien.maulny@gmail.com <julien.maulny@gmail.com@9aab6d8c-2e8b-8920-8967-d62040d22f21>
2013-04-09 10:38:53 +0000

    fonctions personnelles

---
+++ trunk/src/perso/PersonalFunctions.java
@@ -0,0 +1,46 @@
+package perso;
+
+import model.Fonction;
+
+
+/**
+ * 
+ * @author Julien
+ * 
+ * Classe personnelle qui permet d'ajouter des fonctions de periode.
+ * Elles apparaitront dans l'interface graphique, onglet Fonctions.
+ * En cliquant dessus, la période sera recalculée en utilisant la fonction de l'utilisateur.
+ * 
+ * La fonction doit avoir comme signature :
+ * 	
+ * 		public static Fonction fonctionName();
+ * 
+ * 
+ * Elle retourne un objet implementant l'interface Fonction, c'est à dire
+ * un object implementant la fonction double f(double);
+ * 
+ * En entrée : un double entre 0 et 1,
+ * En sortie : un double entre -1 et 1;
+ * 
+ * La période allant de l'abscisse 0 à 1,
+ * et son ordonnée allant de -1 à 1.
+ *
+ */
+public class PersonalFunctions {
+	
+	/**
+	 * 
+	 * @return Fonction objet
+	 */
+	public static Fonction maFonctionPersonnelle() {
+		return new Fonction() {
+			
+			public double f(double x) {
+				return x - 0.5;
+			}
+		};
+	}
+	
+	
+	
+}
