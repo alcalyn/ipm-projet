f2accdf38c25b5d5da5b592fd874efcb054f42ca
julien.maulny@gmail.com <julien.maulny@gmail.com@9aab6d8c-2e8b-8920-8967-d62040d22f21>
2013-04-11 07:18:38 +0000

    export core functions

--- trunk/src/model/Fonctions.java
+++ trunk/src/model/Fonctions.java
@@ -64,71 +64,11 @@ public class Fonctions {
 	}
 	
 	public static ArrayList<FonctionNamed> getCoreFunctions() {
-		return getFonctions(Fonctions.class);
+		return getFonctions(CoreFunctions.class);
 	}
 	
 	public static ArrayList<FonctionNamed> getPersonalFunctions() {
 		return getFonctions(PersonalFunctions.class);
 	}
 	
-	
-	public static Fonction sinusoide() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				return Math.sin(x * Math.PI * 2);
-			}
-		};
-	}
-	
-	public static Fonction carree() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				return x < 0.5 ? -1 : 1;
-			}
-		};
-	}
-	
-	public static Fonction triangle() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				if(x < 0.25) {
-					return (x * 4);
-				} else if(x < 0.75) {
-					return (2 * (x - 0.25) * -2) + 1;
-				} else {
-					return ((x - 0.75) * 4) - 1;
-				}
-			}
-		};
-	}
-	
-	public static Fonction dentsDeScie() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				return x * 2 - 1;
-			}
-		};
-	}
-	
-	public static Fonction silence() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				return 0;
-			}
-		};
-	}
-	
-	public static Fonction bruit() {
-		return new Fonction() {
-			
-			public double f(double x) {
-				return (Math.random() * 2) - 1;
-			}
-		};
-	}
 }
